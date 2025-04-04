6d17de25e4af9b698856058c53cdc7250e72041a
Myo Aung <myoaungg213@gmail.com>
2025-04-03 19:00:06 -0700

    cmake

---
+++ build/CMakeFiles/shop_interface.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/myo/Desktop/Honors-Project-Shop-Interface/Item.cpp" "CMakeFiles/shop_interface.dir/Item.cpp.o" "gcc" "CMakeFiles/shop_interface.dir/Item.cpp.o.d"
+  "/Users/myo/Desktop/Honors-Project-Shop-Interface/StoreInventory.cpp" "CMakeFiles/shop_interface.dir/StoreInventory.cpp.o" "gcc" "CMakeFiles/shop_interface.dir/StoreInventory.cpp.o.d"
+  "/Users/myo/Desktop/Honors-Project-Shop-Interface/User.cpp" "CMakeFiles/shop_interface.dir/User.cpp.o" "gcc" "CMakeFiles/shop_interface.dir/User.cpp.o.d"
+  "/Users/myo/Desktop/Honors-Project-Shop-Interface/main.cpp" "CMakeFiles/shop_interface.dir/main.cpp.o" "gcc" "CMakeFiles/shop_interface.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/shop_interface.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shop_interface.dir/Item.cpp.o"
+  "CMakeFiles/shop_interface.dir/Item.cpp.o.d"
+  "CMakeFiles/shop_interface.dir/StoreInventory.cpp.o"
+  "CMakeFiles/shop_interface.dir/StoreInventory.cpp.o.d"
+  "CMakeFiles/shop_interface.dir/User.cpp.o"
+  "CMakeFiles/shop_interface.dir/User.cpp.o.d"
+  "CMakeFiles/shop_interface.dir/main.cpp.o"
+  "CMakeFiles/shop_interface.dir/main.cpp.o.d"
+  "shop_interface"
+  "shop_interface.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shop_interface.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
